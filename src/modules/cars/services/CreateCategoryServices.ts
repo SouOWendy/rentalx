87e97a80d2c693dcd21d54c3d964d4db3c963b62
Wendy <128555274+SouOWendy@users.noreply.github.com>
2023-04-03 19:42:52 -0300

    Organize the files

--- src/modules/cars/services/CreateCategoryServices.ts
+++ src/modules/cars/services/CreateCategoryServices.ts
@@ -5,11 +5,6 @@ interface IRequest {
   description: string;
 }
 
-/**
- * [X] - Definir o tipo de retorno
- * [X] - Alterar o retorno de erro
- * [X] - Acessar o repositório
- */
 class CreateCategoryService {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
---
+++ src/modules/cars/services/CreateSpecificationServices.ts
@@ -0,0 +1,8 @@
+
+class CreateSpecificationServices {
+  execute() {
+    console.log("ToDo");
+  }
+}
+
+export { CreateSpecificationServices };
