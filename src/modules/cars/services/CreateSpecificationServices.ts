507828ba5bcf00ffca3d8770c4f47de8feda9b6f
Wendy <128555274+SouOWendy@users.noreply.github.com>
2023-04-04 11:46:03 -0300

    Create specification repository

--- src/modules/cars/services/CreateSpecificationServices.ts
+++ src/modules/cars/services/CreateSpecificationServices.ts
@@ -1,7 +1,17 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
 
 class CreateSpecificationServices {
-  execute() {
-    console.log("ToDo");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({name, description}: IRequest): void {
+    if (this.specificationsRepository.findByName(name)) // specification already exists
+      throw new Error("Specification already exists");
+    this.specificationsRepository.create({name, description});
   }
 }
 
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { CreateSpecificationServices } from "../modules/cars/services/CreateSpecificationServices";
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const createSpecificationServices = new CreateSpecificationServices(specificationsRepository);
+
+  createSpecificationServices.execute({name, description});
+
+  return res.status(201).send();
+})
+
+specificationsRoutes.get("/", (req, res) => {
+  return res.json(specificationsRepository.list());
+});
+
+export { specificationsRoutes };
